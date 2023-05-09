659e4c44fe4892a7e9d71ba7dd4088e45de94c0e
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-05-09 11:52:08 +0530

    Expense Items

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+      <div >
+        <h2>Expense Items!</h2>
+        <ul>Food Rs 10</ul>
+        <ul>Petrol Rs 100</ul>
+        <ul>Movie Rs 200</ul>
+      </div>
+    );
+  }
+  
+  export default ExpenseItem;
+  
