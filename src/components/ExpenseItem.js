74c9671788d1b23e27079b827fef0d87267267fd
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-05-09 17:49:15 +0530

    dynamic changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,7 +7,6 @@ function ExpenseItem(props) {
       <div className="expense-item__description"></div>
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount} </div>
-     
       </div>
     );
   } 
