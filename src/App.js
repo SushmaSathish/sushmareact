1f396541d7e3f1f0ed1fee9b02b61a6ae21d6ed6
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-05-10 11:19:07 +0530

    dynamic chnages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses =[{title:"car insurance",amount:240, date:new Date(2023,5,9)},{title:"Food",amount:200, date:new Date(2023,5,10)},{title:"petrol",amount:500, date:new Date(2023,5,10)}]
   return (
-    <div >
+    <div>
       <h2>Lets get started</h2>
       <ExpenseItem title={expenses[0].title}amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
       <ExpenseItem title={expenses[1].title}amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
