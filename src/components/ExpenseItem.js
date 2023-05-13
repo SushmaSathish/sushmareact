fde52f462013362d4a8b6b5006e29e35b2fd9762
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-05-13 11:52:32 +0530

    adding delete button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from  './ExpenseDate';
+import Card from "./card";
 
 function ExpenseItem(props) {
-  
+   const change = ()=>
+   {
+    console.log("changed")
+   }
+   const Delete = ()=>
+   {
+    console.log("deleted")
+   }
      return (
-      <div className="expense-item">
+      <Card className='expense-item'>
       <ExpenseDate date ={props.date}/>
-      <div className="expense-item__description"></div>
-      <h2 className='expense-item h2'>{props.title}</h2>
+      <div className="expense-item__description"><h2 className='expense-item h2'>{props.title}</h2></div>
       <div className="expense-item__price">${props.amount} </div>
-      </div>
+      
+      <button onClick={change}>Change the title</button>
+      <button onClick={Delete}>Delete</button>
+      </Card>
     );
   } 
   export default ExpenseItem;
