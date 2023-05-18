7b2dee5b6c4d4eb04689593ac05820b4492e626c
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-05-18 14:37:52 +0530

    Filter addons in expense Form

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from  './ExpensesFilter';
+
 import './Expenses.css';
 import Card from "./card";
-function Expenses(props) { 
+const  Expenses=(props)=> { 
+  const [filteredyear,setFilteredyear] = useState('2020');
+  const filterChangeHandler=selectedyear=>{
+    setFilteredyear(selectedyear)
+  }
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredyear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItem title={props.items[0].title}amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
       <ExpenseItem title={props.items[1].title}amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
       <ExpenseItem title={props.items[2].title}amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
