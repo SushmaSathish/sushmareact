8f32d44a8e7f07b42bf6edb82911bd30171725be
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-05-20 13:53:18 +0530

    adding filter dynamically

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from './ExpensesList.js';
 import ExpensesFilter from  './ExpensesFilter';
 
 import './Expenses.css';
 import Card from "./card";
+import ExpenseList from './ExpensesList.js';
 const  Expenses=(props)=> { 
   const [filteredyear,setFilteredyear] = useState('2020');
   const filterChangeHandler=selectedyear=>{
@@ -11,12 +12,16 @@ const  Expenses=(props)=> {
   }
   const filteredExpenses= props.items.filter(expense=>{
     return expense.date.getFullYear().toString()===filteredyear  })
+   
   return (
+    <div>
     <Card className="expenses">
       <ExpensesFilter selected={filteredyear} onChangeFilter={filterChangeHandler}/>
-  
-      {filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
+      {/*filteredExpenses.length===0 && <p>No expenses found.</p>}
+      {filteredExpenses.length>0 &&( filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>))*/}
+      <ExpenseList items={filteredExpenses}/>
     </Card>
+    </div>
   );
 }
 
--- src/components/ExpensesFilter.js
+++ src/components/ExpensesFilter.js
@@ -10,8 +10,8 @@ const ExpensesFilter = (props) => {
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
-        <label>Filter by year</label>
-        <select value={props.selected} onChange={dropDownHandler}>
+        <label className="expenses-filter label">Filter by year</label>
+        <select  value={props.selected} onChange={dropDownHandler}>
           <option value='2023'>2023</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
