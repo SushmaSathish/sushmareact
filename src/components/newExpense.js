e9610f33b9033ada70c9cda15d636b2c5df96d13
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-05-20 14:52:25 +0530

    adding add expense and hide the add form

--- src/components/newExpense.js
+++ src/components/newExpense.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Expenseform from './Expenseform';
 import './NewExpense.css'
 
 const NewExpense=(props)=>{
+    const [isEditing, setIsEditing]= useState(false);
     const saveExpenseHandler=(enteredexpensedata)=>{
-       const expenseData={ ...enteredexpensedata,
+       const expenseData={...enteredexpensedata,
         id:Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
     }
+    const startEditingHandler=()=>{
+        setIsEditing(true);
+    };
+    const stopEditingHandler=()=>{
+        setIsEditing(false);
+    };
 return (
 <div className='new-expense'>
-<Expenseform onSaveExpenseData={saveExpenseHandler}/>
+   {!isEditing &&<button onClick={startEditingHandler}>Add new expense</button>}
+   {isEditing &&<Expenseform onSaveExpenseData={saveExpenseHandler} onCancel={stopEditingHandler}/>}
+
 </div>)
 }
 export default NewExpense;
